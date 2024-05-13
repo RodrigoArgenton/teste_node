966782a859942129459cd972b26c26a49e9369d6
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-05-13 08:33:23 -0300

    metodos e url básicos do http

--- src/server.js
+++ src/server.js
@@ -2,6 +2,23 @@ import http from 'node:http'
 
 // criando o servidor
 const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('Lista de usuários...')
+    }
+    if(method === 'POST' && url === '/users'){
+        return res.end('Criação de usuário...')
+    }
+    if(method === 'PUT' && url === '/users'){
+        return res.end('Edição de usuários concluída...')
+    }
+    if(method === 'PATCH' && url === '/users'){
+        return res.end('Termos editado...')
+    }
+    if(method === 'DELETE' && url === '/users'){
+        return res.end('Usuário deletado...')
+    }
     return res.end('Hello World')
 })
 
