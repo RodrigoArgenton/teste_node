6781f9f55540d1e2f2f4602738fe43a263ded977
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-05-13 09:08:21 -0300

    criação e exibição de dados no GET e POST

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
 import http from 'node:http'
 
+// const que salva, temporariamente, os dados da aplicação
+const users = []
+
 // criando o servidor
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
     if(method === 'GET' && url === '/users'){
-        return res.end('Lista de usuários...')
+        return res
+            // Com essa função, o valor retornado fica em formato JSON
+            .setHeader('Content-type', 'application/json')
+            // Transforma um array em JSON
+            .end(JSON.stringify(users))
     }
     if(method === 'POST' && url === '/users'){
-        return res.end('Criação de usuário...')
+        // Metódo para enviar informações para a memoria
+        users.push({
+            id: 1,
+            nome: 'Rodrigo Argenton Barbosa',
+            email: 'rodrigolk4321@gmail.com',
+        })
+
+        return res.end('Usuário criado...')
     }
     if(method === 'PUT' && url === '/users'){
         return res.end('Edição de usuários concluída...')
