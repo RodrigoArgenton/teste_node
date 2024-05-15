733a25e34b5ec4764023d47601b97f9abdd98222
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-05-15 15:04:06 -0300

    atualização de dependências

--- src/server.js
+++ src/server.js
@@ -1,52 +1,73 @@
 import http from 'node:http'
 import url from 'url'
-import { randomUUID }  from 'node:crypto';
+import { MongoClient } from 'mongodb';
 
-// const que salva, temporariamente, os dados da aplicação
-const users = []
+// conexão com o banco de dados
+const uri = 'mongodb+srv://rodrigolk4321:R2RvL2gNAmw4I1uj@users.qzojpzs.mongodb.net/?retryWrites=true&w=majority&appName=users'
+const client = new MongoClient(uri)
+const database = client.db('database')
+const collection = database.collection('users')
 
 // criando o servidor
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     const method = req.method
     const parsedUrl = url.parse(req.url, true)
     const { query, pathname } = parsedUrl
+    const name = query.name
+    const email = query.email
     
+
     if(method === 'GET' && pathname === '/users'){
+        const dados = await searchDB()
         return res
             // Com essa função, o valor retornado fica em formato JSON
             .setHeader('Content-type', 'application/json')
-            // Transforma um array em JSON
-            .end(JSON.stringify(users))
+            .end(JSON.stringify(dados))
     }
     if(method === 'POST' && pathname === '/users'){
-        const name = query.name
-        const email = query.email
-        const id = randomUUID()
         //validação de dados enviados
-        if(name || email){
-            return res.writeHead(400).end("Um ou mais campos, não foram preenchidos. Campo obrigatórios: name e email.")
+        if(!name || !email){
+            return res.writeHead(400).end('Um ou mais campos, não foram preenchidos. Campo obrigatórios: name e email.')
         }
-        // Metódo para enviar informações para a memoria
-        users.push({
-            id,
-            name,
-            email
-        })
+        
+        await insertDB(name, email)
         // retorna o status code 201 (created)
-        return res.writeHead(201).end()
+        return res.writeHead(201).end('Cadastro realizado com sucesso')
     }
-    if(method === 'PUT' && pathname === '/users'){
-        return res.end('Edição de usuários concluída...')
-    }
-    if(method === 'PATCH' && pathname === '/users'){
-        return res.end('Termos editado...')
-    }
-    if(method === 'DELETE' && pathname === '/users'){
-        return res.end('Usuário deletado...')
-    }
+    // if(method === 'PUT' && pathname === '/users'){
+    //     return res.end('Edição de usuários concluída...')
+    // }
+    // if(method === 'PATCH' && pathname === '/users'){
+    //     return res.end('Termos editado...')
+    // }
+    // if(method === 'DELETE' && pathname === '/users'){
+    //     return res.end('Usuário deletado...')
+    // }
     //retorna o status code 404 (not found)
     return res.writeHead(404).end()
 })
 
+async function searchDB (){
+    try{
+        const result = await collection.find({}).toArray()
+        console.log(result)
+        return result
+    }catch(err){
+        console.error('Erro ao buscar dados: ', err)
+        throw err
+    }
+}
+async function insertDB(name, email){
+    try{
+        const data = {
+            name,
+            email
+        }
+        const insertData = await collection.insertOne(data)
+    }catch{
+        console.error('Erro ao inserir documento:', err)
+        throw err
+    }
+}
 //passando a porta
 server.listen(3333)
