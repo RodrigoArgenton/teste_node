65b4c5dbee6ea37ff41d5d718992871518ab352e
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-05-20 13:27:35 -0300

    atualização de estrutura, projeto foi construido em cima do expressa

--- src/server.js
+++ src/server.js
@@ -1,86 +1,37 @@
-import http from 'node:http'
-import url from 'url'
-import { MongoClient } from 'mongodb'
-import dotenv from 'dotenv';
+import express from 'express'
+import {searchDB, insertDB} from '../database/dbUser.js'
 
-dotenv.config();
+const app = express()
+// Middleware para analisar o corpo das requisições como JSON
+app.use(express.json())
 
-// conexão com o banco de dados
-
-
-// criando o servidor
-const server = http.createServer(async(req, res) => {
-    const method = req.method
-    const parsedUrl = url.parse(req.url, true)
-    const { query, pathname } = parsedUrl
-    const name = query.name
-    const email = query.email
-    
-
-    if(method === 'GET' && pathname === '/users'){
-        const dados = await searchDB()
-        return res
-            // Com essa função, o valor retornado fica em formato JSON
-            .setHeader('Content-type', 'application/json')
-            .end(JSON.stringify(dados))
-    }
-    if(method === 'POST' && pathname === '/users'){
-        //validação de dados enviados
-        if(!name || !email){
-            return res.writeHead(400).end('Um ou mais campos, não foram preenchidos. Campo obrigatórios: name e email.')
+try{
+    app.get('/users', async(req, res) => {
+        try{
+            const users = await searchDB()
+            return res
+                .setHeader('content-type', 'application/json')
+                .end(JSON.stringify(users))
+        }catch(err){
+            res.writeHead(500).end(err)
         }
-        
-        await insertDB(name, email)
-        // retorna o status code 201 (created)
-        return res.writeHead(201).end('Cadastro realizado com sucesso')
-    }
-    // if(method === 'PUT' && pathname === '/users'){
-    //     return res.end('Edição de usuários concluída...')
-    // }
-    // if(method === 'PATCH' && pathname === '/users'){
-    //     return res.end('Termos editado...')
-    // }
-    // if(method === 'DELETE' && pathname === '/users'){
-    //     return res.end('Usuário deletado...')
-    // }
-    //retorna o status code 404 (not found)
-    return res.writeHead(404).end()
-})
+    })
+    app.post('/users', async(req, res) => {
+        const { name, email} = req.query
 
-async function searchDB (){
-    try{
-        const collection = connectionDB()
-        const result = await collection.find({}).toArray()
-        return result
-    }catch(err){
-        console.error('Erro ao buscar dados: ', err)
-        throw err
-    }
-}
-async function insertDB(name, email){
-    try{
-        const collection = connectionDB()
-        const data = {
-            name,
-            email
+        try{
+            await insertDB(name, email)
+            res.writeHead(201).end('Cadastro realizado com sucesso!')
+        }catch(err){
+            res.writeHead(500).end(err)
         }
-        const insertData = await collection.insertOne(data)
-    }catch{
-        console.error('Erro ao inserir documento:', err)
-        throw err
-    }
+
+    })
+    // configuração de porta
+    const PORT = process.env.PORT || 3333
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`)
+    })
+}catch(err){
+    console.log(err)
 }
-function connectionDB(){
-    try{
-        const uri = process.env.URI
-        const client = new MongoClient(uri)
-        const database = client.db('database')
-        const collection = database.collection('users')
-        return collection
-    }catch(err){
-        console.log('Não foi possivel conectar devido ao erro: ', err)
-        throw(err)
-    }
-}
-//passando a porta
-server.listen(3333)
