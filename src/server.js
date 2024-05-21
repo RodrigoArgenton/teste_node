6df01146b071ed108a77faa054b5b46684982a0d
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-05-21 08:44:49 -0300

    integração com joi para validação de dados

--- src/server.js
+++ src/server.js
@@ -1,37 +1,48 @@
-import express from 'express'
-import {searchDB, insertDB} from '../database/dbUser.js'
+import express from 'express';
+import { searchDB, insertDB } from '../database/dbUser.js';
+import validationUser from '../validator/controllers.js'; // Ajuste o caminho conforme necessário
 
-const app = express()
+const app = express();
 // Middleware para analisar o corpo das requisições como JSON
-app.use(express.json())
+app.use(express.json());
 
-try{
-    app.get('/users', async(req, res) => {
-        try{
-            const users = await searchDB()
-            return res
-                .setHeader('content-type', 'application/json')
-                .end(JSON.stringify(users))
-        }catch(err){
-            res.writeHead(500).end(err)
+app.get('/users', async (req, res) => {
+    try {
+        const users = await searchDB();
+        return res.setHeader('Content-Type', 'application/json').json(users);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    const { name, email } = req.body;
+
+    try {
+        const schema = validationUser();
+        await schema.validateAsync(req.body, { abortEarly: false }); // abortEarly= false, faz com que não retorne somente um erro, caso exista mais de um 
+
+        await insertDB(name, email);
+        return res.status(201).json({ message: 'Cadastro realizado com sucesso!' });
+    } catch (err) {
+        if (err.isJoi) {
+            // Retorna os erros existentes
+            return res.status(400).json({
+                error: err.details.map(detail => ({
+                    message: detail.message,
+                    path: detail.path,
+                }))
+            });
+        } else {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro interno do servidor' });
         }
-    })
-    app.post('/users', async(req, res) => {
-        const { name, email} = req.query
+    }
+});
 
-        try{
-            await insertDB(name, email)
-            res.writeHead(201).end('Cadastro realizado com sucesso!')
-        }catch(err){
-            res.writeHead(500).end(err)
-        }
-
-    })
-    // configuração de porta
-    const PORT = process.env.PORT || 3333
-    app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`)
-    })
-}catch(err){
-    console.log(err)
-}
+// Configuração de porta
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
