a143532b09cba9867fd3263111bed833854bae56
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-03 08:04:45 -0300

    Retorno de possiveis erros ao enviar dados

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,25 +1,38 @@
-// import { userModel } from "../models/userModel";
-import { Request, Response } from "express";
-import { getUsersDB, postUsersDB } from "../database/db";
+// import { userModel } from "../models/userModel"
+import { Request, Response } from "express"
+import { getUsersDB, postUsersDB } from "../database/db"
+import validationUser from "../middlewares/validatorUser"
+
 
 export async function getUsers(req:Request, res:Response):Promise<Response>{
     try{
-        const users = await getUsersDB();
-        return res.status(200).json(users);
+        const filter = req.params.name
+        console.log(filter)
+        const users = await getUsersDB(filter)
+        return res.status(200).json(users)
     }catch(err){
         console.log(err)
         return res.status(500).send('Erro ao obter usuários')
     }
-};
+}
 
 export async function postUsers(req:Request, res:Response):Promise<Response>{
     try {
-        const name = req.body.name;
-        const email = req.body.email;
-        const users = await postUsersDB(name, email)
+        const schema = validationUser()
+        await schema.validateAsync(req.body, { abortEarly: false}) 
+        const { name, email } = req.body
+        await postUsersDB(name, email)
         return res.status(201).send('Cadastro criado!')
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send('Erros ao criar cadastro...')
+    } catch (err: any) {
+        if (err.isJoi){
+            return res.status(400).json({
+                error: err.details.map((detail: any) => ({
+                    message: detail.message,
+                    path: detail.path,
+                }))
+            })
+        }else{
+            return res.status(500).json('Erro interno')
+        }
     }
 }
